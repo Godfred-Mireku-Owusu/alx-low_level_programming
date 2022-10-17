eb004d6284b15fd54a73872c8c5e13b7c026067d
Godfred-Mireku-Owusu <godfredmirekuowusu@gmail.com>
2022-10-17 02:29:59 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the description here'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int num;
+
+for (num = 0; num <= 9; num++)
+printf("%d", num);
+return (0);
+}
