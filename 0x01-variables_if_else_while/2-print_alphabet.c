afd57bef68795c66ccd8aa77e37f8bdfb0f0d409
Godfred-Mireku-Owusu <godfredmirekuowusu@gmail.com>
2022-10-17 02:10:07 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the description here'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int alph;
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+		putchar(alph);
+	return (0);
+}
