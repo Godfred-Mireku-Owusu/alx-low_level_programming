cbda1d4ce99297f233f3ccfb1ff000600b2d61eb
Godfred-Mireku-Owusu <godfredmirekuowusu@gmail.com>
2022-10-18 02:57:54 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* Main - tebahpla
+*
+*Description: 'the description is here'
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int x;
+
+for (x = 48; x <= 57; x++) 
+{ 
+putchar(x);
+putchar(44);
+putchar(' ');
+} 
+return (0);  
+}
