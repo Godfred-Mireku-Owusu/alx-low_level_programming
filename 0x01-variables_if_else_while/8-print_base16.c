bde03ea1b881fb3625294a0790c9832b840570c3
Godfred-Mireku-Owusu <godfredmirekuowusu@gmail.com>
2022-10-18 02:23:03 -0700

    8-print_base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* Main - tebahpla
+*
+*Description: 'the description is here'
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int x;
+for (x = 48; x <= 57; x++)
+putchar(x);
+for (x = 'a'; x <= 'f'; x++)
+putchar(x);
+putchar('\n');
+return (0);
+}
