b8ecef2e805b1cfde94901d3de5807c8a51bec46
Godfred-Mireku-Owusu <godfredmirekuowusu@gmail.com>
2022-10-19 17:07:03 -0700

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,25 +1,19 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - prints the lowercase alphabet x10 with _putchar
+ * print_alphabet - prints the lowercase alphabets with _putchar.
  *
  * Return: void
  */
 
-void print_alphabet_x10(void)
+void print_alphabet(void)
 {
 	char alphabet = 'a';
-	int counter = 0;
 
-	while (counter < 10)
+	while (alphabet < 123)
 	{
-		while (alphabet < 123)
-		{
-			_putchar(alphabet);
-			alphabet++;
-		}
-		alphabet = 'a';
-		_putchar(10);
-		counter++;
+		_putchar(alphabet);
+		alphabet++;
 	}
+	_putchar(10);
 }
